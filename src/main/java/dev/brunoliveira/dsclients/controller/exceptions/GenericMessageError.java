32532837eccc22ee66a20025fcf83584b9ebe8ec
brunoliveiradev <bruno.henrique0liveir4@gmail.com>
2021-08-17 20:36:11 -0300

    Method Not Valid Exception - Handler created (HttpStatus.Bad_Request (400))

---
+++ src/main/java/dev/brunoliveira/dsclients/controller/exceptions/GenericMessageError.java
@@ -0,0 +1,23 @@
+package dev.brunoliveira.dsclients.controller.exceptions;
+
+import java.io.Serializable;
+
+public class GenericMessageError implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String field;
+    private String message;
+
+    public GenericMessageError(String campo, String mensagemErro) {
+        this.field = campo;
+        this.message = mensagemErro;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
